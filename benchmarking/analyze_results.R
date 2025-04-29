f7bb6646150822807b21c184ae3e3cbb1fd4978d
sjpi22 <124000054+sjpi22@users.noreply.github.com>
2025-04-28 22:00:56 -0700

    Update benchmark result plots for prevalence

--- benchmarking/analyze_results.R
+++ benchmarking/analyze_results.R
@@ -28,28 +28,38 @@ sapply(distr.sources, source, .GlobalEnv)
 #### 2. General parameters ========================================================
 
 ###### 2.1 General parameters
-path_benchmark <- "benchmarking/result.rds" # Path to save results
 v_labels <- c("cs" = "Cross-sectional", "long" = "Longitudinal", "rcs" = "Repeated cross-sectional")
 dodge_width_factor <- 0.7
 error_width_factor <- 0.4
 plt_size_text <- 18
 plt_size_small <- 5/6*plt_size_text
-path_prevalence <- "benchmarking/result_prevalence.pdf"
+path_benchmark <- "benchmarking/benchmark_output.rds" # Path to save results
+path_output <- "benchmarking/result_prevalence.pdf"
 
 #### 3. Pre-processing  ===========================================
 
 # Load results
 res <- readRDS(path_benchmark)
 
+# Remove incidence
+res$outcomes <- lapply(res$outcomes, function(x) x[method != "incidence"])
+res$time <- lapply(res$time, function(x) x[!grepl("incidence", names(x))])
+
 # Calculate maximum time
-max_time <- max(unlist(res$time))
+max_time <- max(unlist(lapply(res$time, function(x) do.call(rbind, x)[, "mean"])))
 
 #### 4. Analysis  ===========================================
 
 dt_stats <- dt_time <- plt_stats <- plt_time <- list()
 for (i in 1:length(res$time)) {
+  if (i == 1) {
+    xlab = "Single interval from age 30 to 80"
+  } else {
+    xlab = "10-year intervals from age 30 to 80"
+  }
+  
   # Bar plot of time
-  dt_time[[i]] <- res$time[[i]]
+  dt_time[[i]] <- do.call(rbind, res$time[[i]])
   
   # Get stats
   dt_stats[[i]] <- res$outcomes[[i]][, `:=` (age_range = age_end - age_start,
@@ -61,7 +71,7 @@ for (i in 1:length(res$time)) {
   dodge_width <- dodge_width_factor*dt_stats[[i]]$age_range[1]
   error_width <- error_width_factor*dt_stats[[i]]$age_range[1]
   plt_stats[[i]] <- ggplot(dt_stats[[i]], 
-                      aes(x = age_median, y = mean_scaled, color = method, fill = method)) +
+                           aes(x = age_median, y = mean_scaled, color = method, fill = method)) +
     geom_hline(yintercept = 0, linetype = "dashed") +
     geom_point(position = position_dodge(width = dodge_width), size = 3) +
     geom_errorbar(aes(ymin = mean_scaled - sd_scaled, ymax = mean_scaled + sd_scaled), 
@@ -71,9 +81,7 @@ for (i in 1:length(res$time)) {
     scale_color_hue(h = c(180, 300), labels = v_labels, guide = "none") +
     scale_fill_hue(h = c(180, 300), labels = v_labels, guide = "none") +
     labs(x = "Age",
-         y = "% Difference",
-         color = "Formulation",
-         fill = "Formulation") +
+         y = "Percentage difference") +
     theme_bw() + 
     theme(plot.title = element_blank(),
           axis.text.x = element_text(size = plt_size_small),
@@ -84,18 +92,20 @@ for (i in 1:length(res$time)) {
           legend.text = element_text(size = plt_size_small))
   
   # Bar plot of time
-  plt_time[[i]] <- ggplot(data.frame(time = dt_time[[i]], method = names(dt_time[[i]])), 
-                     aes(x = method, y = time, fill = method)) +
-    geom_bar(stat = "identity", width = 0.5) +
+  plt_time[[i]] <- ggplot(data.frame(dt_time[[i]], method = rownames(dt_time[[i]])), 
+                          aes(x = method, y = mean, fill = method)) +
+    geom_errorbar(aes(ymin = mean - sd, ymax = mean + sd), width = 0.5) +
+    geom_bar(stat = "identity", width = 0.7) +
     scale_fill_hue(h = c(180, 300), labels = v_labels) +
     coord_cartesian(ylim = c(0, max_time)) +
     labs(fill = "Formulation",
+         x = xlab,
          y = "Seconds") +
     theme_bw() + 
     theme(plot.title = element_blank(),
           axis.text.x = element_blank(),
           axis.text.y = element_text(size = plt_size_small),
-          axis.title.x = element_blank(),
+          axis.title.x = element_text(size = plt_size_small),
           axis.title.y = element_text(size = plt_size_small),
           legend.title = element_text(size = plt_size_small),
           legend.text = element_text(size = plt_size_small))
@@ -103,17 +113,17 @@ for (i in 1:length(res$time)) {
 
 
 # Plot final patchwork
-((wrap_elements(textGrob('Mean and standard error of estimated vs. true prevalence', gp = gpar(fontsize = plt_size_text))) /
+((wrap_elements(textGrob('Mean difference from true prevalence', gp = gpar(fontsize = plt_size_text))) /
     (plt_stats[[1]] + plt_stats[[2]] + 
-     plot_layout(axis_titles = "collect", guides = "collect")) /
-    wrap_elements(textGrob('Time to run 1,000 simulations', gp = gpar(fontsize = plt_size_text)))/
+       plot_layout(axis_titles = "collect", guides = "collect")) /
+    wrap_elements(textGrob('Mean time', gp = gpar(fontsize = plt_size_text)))/
     (plt_time[[1]] + plt_time[[2]] + 
        plot_layout(axis_titles = "collect", guides = "collect"))) +
-  plot_layout(axis_titles = "collect", guides = "collect",
-              heights = c(0.2, 1, 0.2, 1)) & theme(legend.position = "bottom")) +
+    plot_layout(axis_titles = "collect", guides = "collect",
+                heights = c(0.2, 1, 0.2, 1)) & theme(legend.position = "bottom")) +
   plot_annotation(title = "",
                   theme = theme(plot.title = element_text(size = plt_size_text, face = "bold")))
-                    
+
 # Save plot
-ggsave(path_prevalence, 
+ggsave(path_output, 
        width = 9, height = 8, dpi = 300, units = "in")
